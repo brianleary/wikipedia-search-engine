a0421973a3cc692e499da1b221fe23f27a0d27bc
Brian Leary <36583028+brianleary@users.noreply.github.com>
2023-11-12 17:11:35 -0600

    Add files via upload

---
+++ gui/addScript.js
@@ -0,0 +1,89 @@
+/*jslint browser: true, indent: 3 */
+/*jshint esversion: 6 */
+
+document.addEventListener('DOMContentLoaded', function () {
+
+   // Enforce stricter JavaScript rules.
+   'use strict';
+
+   // Declare this function's local variables.
+   var idInputElement, titleInputElement, textInputElement, submitArticleButton, articleOutputElement;
+
+   // Find all needed elements and save them in variables.
+   idInputElement = document.querySelector('#id-input');
+   titleInputElement = document.querySelector('#title-input');
+   textInputElement = document.querySelector('#text-input');
+   submitArticleButton = document.querySelector('#submit-article');
+   articleOutputElement = document.querySelector('#article-output');
+
+   // Check if string only contains numbers
+   function isNum(string) {
+      return /^[0-9]*$/.test(string);
+   }
+
+   // API Fetch Function
+   function addArticleAPICall(url, dataToSend) {
+      fetch(url, {
+         method: 'POST',
+         body: JSON.stringify(dataToSend),
+         mode: 'cors',
+         headers: new Headers({
+            'Content-Type': 'application/json'
+         })
+      })
+      // Convert response to JSON
+      .then(response => {
+         return response.json();
+      })
+      .then(data => {
+         // Log response to console
+         console.log('Response', data);
+
+         // Check if article was added successfully
+         if (data.result === "created") {
+            articleOutputElement.textContent = "Article added succesfully!";
+         } else {
+            articleOutputElement.textContent = "Error adding article";
+         }
+      })
+      // Catch and log error to console
+      .catch(error => console.log(error));
+   }
+
+   // Function called when searching (called by eventlisteners)
+   function addArticleFunction() {
+      var id, title, text, dataToSend, url;
+
+      url = "http://localhost:9200/articlesindex/_doc/";
+
+      // Get the string values out of the input textboxes.
+      id = idInputElement.value;
+      title = titleInputElement.value;
+      text = textInputElement.value;
+
+      if (id.length === 0) {
+         // No ID inputted
+         articleOutputElement.textContent = 'Please type in an article ID';
+      } else if (!isNum(id)) {
+         // ID is non-integer
+         articleOutputElement.textContent = 'IDs must only be numeric characters';
+      } else if (title.length === 0) {
+         // No title inputted
+         articleOutputElement.textContent = 'Please type in a title';
+      } else if (text.length === 0) {
+         // No text inputted
+         articleOutputElement.textContent = 'Please type in text for the article';
+      } else {
+         // Add article
+         articleOutputElement.textContent = "all fields are valid";
+         dataToSend = {"docid": Number(id), "title": title, "text": text};
+         addArticleAPICall(url, dataToSend)
+      }
+   }
+
+   // Handle clicking on button
+   submitArticleButton.addEventListener('click', function () {
+      addArticleFunction();
+   }, false);
+
+}, false);
---
+++ gui/searchScript.js
@@ -0,0 +1,101 @@
+/*jslint browser: true, indent: 3 */
+/*jshint esversion: 6 */
+
+document.addEventListener('DOMContentLoaded', function () {
+
+   // Enforce stricter JavaScript rules.
+   'use strict';
+
+   // Declare this function's local variables.
+   var queryInputElement, queryOutputElement, submitqueryButton;
+
+   // Find all needed elements and save them in variables.
+   queryInputElement = document.querySelector('#query-input');
+   submitqueryButton = document.querySelector('#submit-query');
+   queryOutputElement = document.querySelector('#query-output');
+
+   // API Fetch Function
+   function searchAPICall(url, dataToSend) {
+      fetch(url, {
+         method: 'POST',
+         body: JSON.stringify(dataToSend),
+         mode: 'cors',
+         headers: new Headers({
+            'Content-Type': 'application/json'
+         })
+      })
+      // Convert response to JSON
+      .then(response => {
+         return response.json();
+      })
+      .then(data => {
+         // Log response to console
+         console.log('Response', data);
+
+         // Clear output for each search
+         queryOutputElement.textContent = "";
+
+         if (data.hits.hits.length === 0) {
+            // If array size is zero, there were no search results
+            queryOutputElement.textContent = "No results";
+         } else {
+            // There were results
+
+            // Create table header
+            queryOutputElement.insertAdjacentHTML('beforeend', `<th class="tableCell">Document Score</td>
+                                                                <th class="tableCell">Document Title</td>
+                                                                <th class="tableCell">Link</td>
+                                                                <th class="tableCell">Text Preview</td>`);
+
+            // Access data.hits.hits, which is an array of the search results
+            // Loop through each result and create a row in the table
+            data.hits.hits.map(searchResult => {
+               // Create table row element
+               var urlString = "https://en.wikipedia.org/wiki/" + searchResult._source.title.replace(' ', '_');
+               const markup = `<td class="tableCell">${searchResult._score}</td>
+                               <td class="tableCell">${searchResult._source.title}</td>
+                               <td class="tableCell"><a href=${urlString}>Go to page</a></td>
+                               <td class="tableCell">${searchResult._source.text.substring(0, 250)}</td>`;
+               queryOutputElement.insertAdjacentHTML('beforeend', markup);
+            });
+         }
+      })
+      // Catch and log error to console
+      .catch(error => console.log(error));
+   }
+
+   // Function called when searching (called by eventlisteners)
+   function searchQueryFunction() {
+      var query, dataToSend, url;
+
+      url = "http://localhost:9200/articlesindex/_search";
+
+      // Get the string value out of the input textbox.
+      query = queryInputElement.value;
+
+      if (query.length === 0) {
+         // No query inputted
+         queryOutputElement.textContent = 'Please type in a search query';
+      } else {
+         // Query inputted
+         // Create JSON object for search query
+         dataToSend = {"query": { "match": { "text": { "query": query }}}};
+
+         //result = searchAPICall(url, dataToSend)
+         searchAPICall(url, dataToSend);
+      }
+   }
+
+   // Handle pressing enter on textbox
+   queryInputElement.addEventListener('keypress', function (e) {
+      if (e.key === 'Enter') {
+         searchQueryFunction();
+      }
+   }, false);
+
+   // Handle clicking on button
+   submitqueryButton.addEventListener('click', function () {
+      searchQueryFunction();
+   }, false);
+
+}, false);
